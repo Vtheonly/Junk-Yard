53a3d070910c81277388c2b34e14518a005efa24
mersel_fares <merselfaresw@gmail.com>
2024-08-26 02:44:38 +0100

    add the clearAudioTags to the github

---
+++ clearAudioTags.py
@@ -0,0 +1,29 @@
+import os
+import eyed3  # type: ignore may or may not be avialalbe  pip first
+directory_path =  r"C:\Users\merse\Music\Music"
+
+def clear_mp3_metadata(file_path):
+    audiofile = eyed3.load(file_path)
+    if audiofile is None:
+        print(f"Failed to load {file_path}.")
+        return
+    audiofile.tag.clear()
+    audiofile.tag.save()
+
+
+
+def clear_metadata_in_directory(directory_path):
+    if not os.path.isdir(directory_path):
+        raise NotADirectoryError(f"The provided path '{directory_path}' is not a directory.")
+    
+    # all mp3 in the directory
+    files = [file for file in os.listdir(directory_path) if file.endswith(".mp3")]
+    
+
+    for file in files:
+        file_path = os.path.join(directory_path, file)
+        clear_mp3_metadata(file_path)
+        print(f"Cleared metadata for {file}")
+
+
+clear_metadata_in_directory(directory_path)
