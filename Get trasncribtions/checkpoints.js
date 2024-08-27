37522fa8fc6358766a250085c264297b9526a21d
mersel_fares <merselfaresw@gmail.com>
2024-08-27 18:38:46 +0100

    no need for checkpoints

--- Get trasncribtions/checkpoints.js
+++
@@ -1,4 +0,0 @@
-const elements = document.querySelectorAll('[id^="title"][class*="ytd-transcript-section-header-renderer"][aria-hidden="true"][tabindex="-1"]');
-const texts = Array.from(elements).map(element => element.textContent.trim());
-
-export default texts;
